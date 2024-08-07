44a40e3d05083f0e34f0e072474697455c8a6110
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-08-07 13:24:11 -0400

    Finished Project

--- src/operatorChallenge.java
+++ src/operatorChallenge.java
@@ -1,2 +1,13 @@
 public class operatorChallenge {
+    public static void main(String[] args) {
+        double value1 = 20;
+        double value2 = 80;
+        double value3 = value1 + value2;
+        double value4 = value3 % 40;
+        boolean value5 = value4 == 0;
+        System.out.println(value5);
+        if (!value5){
+            System.out.println("got some remainder");
+        }
+    }
 }
